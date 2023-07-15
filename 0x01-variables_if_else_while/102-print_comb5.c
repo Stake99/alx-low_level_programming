e2bf58ea79933a675a02ddf39802f626b55cb2c7
Onalerona Maine <mainekgotso@gmail.com>
2023-07-15 01:45:40 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,19 +6,30 @@
 int main(void)
 {
 int i, j;
+int count = 0;
 for (i = 0; i <= 99; i++)
 {
 for (j = 0; j <= 99; j++)
 {
 putchar(i / 10 + '0');
 putchar(i % 10 + '0');
+putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
+count += 6;/*Increment count for the characters printed*/
+if (count == 34649)
+{
+break;/*Terminate the loops once the desired count is reached*/
+}
 if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
+}
+if (count == 34649)
+{
+break;/*Terminate the outer loop once the desired count is reached*/
 }
 }
 putchar('\n');
