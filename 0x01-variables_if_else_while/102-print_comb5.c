e46143332c65dce363251f356bc193f0dc70a30b
Onalerona Maine <mainekgotso@gmail.com>
2023-07-15 01:47:49 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,20 +16,11 @@ putchar(i % 10 + '0');
 putchar(' ');
 putchar(j / 10 + '0');
 putchar(j % 10 + '0');
-count += 6;/*Increment count for the characters printed*/
-if (count == 34649)
-{
-break;/*Terminate the loops once the desired count is reached*/
-}
 if (i != 99 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
-}
-if (count == 34649)
-{
-break;/*Terminate the outer loop once the desired count is reached*/
 }
 }
 putchar('\n');
