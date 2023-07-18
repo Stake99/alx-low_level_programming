68a1925bf89403174c6906faf4f65df859e3ae0c
Onalerona Maine <mainekgotso@gmail.com>
2023-07-18 20:10:29 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,14 +2,7 @@
 /**
 *print_alphabet_10x - prints alphabets 10 times
 *Return: 0 (success)
-* main - check the code for Holberton School students.
-* Return: Always 0.
 */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
 void print_alphabet_10x(void)
 {
 int i;
