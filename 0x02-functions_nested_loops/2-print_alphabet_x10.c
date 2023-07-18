b93ce79a1328b8519d87ef78321c35a600a9f4f1
Onalerona Maine <mainekgotso@gmail.com>
2023-07-18 19:48:22 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+*print_alphabet_10x - prints alphabets 10 times
+*Return: 0 (success)
+*/
+void print_alphabet_10x(void)
+{
+int i;
+int j;
+for (i = 1; i <= 10; i++)
+{
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
