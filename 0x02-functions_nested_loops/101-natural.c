f322297e3321a0186004e4ec1df636e8fa4d1a3f
Onalerona Maine <mainekgotso@gmail.com>
2023-07-19 15:18:45 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,4 +3,16 @@
 *main - entry point
 *Return: 0 (success)
 */
-
+int main(void)
+{
+int i, sum = 0;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
