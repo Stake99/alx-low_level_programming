b4709342bc84fa953be7bf274b129d6559c9165b
Onalerona Maine <mainekgotso@gmail.com>
2023-07-19 12:10:27 +0000

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -48,11 +48,12 @@ void print_time_table(int n)
 		*}
 	}*
 	*/
+int i;
 if (n < 0 || n > 15)
 {
 return;
 }
-for (int i = 0; i <= 10; i++)
+for (i = 0; i <= 10; i++)
 {
 printf("%d x %d = %d\n", n, i, n * i);
 }
