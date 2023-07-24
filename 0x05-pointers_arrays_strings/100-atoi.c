83fb5a0251f43db030cf3015b8cc8f43383f4705
Onalerona Maine <mainekgotso@gmail.com>
2023-07-24 13:50:41 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+int result = 0;
+int sign = 1;
+
+for(; *s; s++)
+{
+	if (*s == '-')
+	{
+		sign *= -1:
+	}
+	else if (*s >= '0' && *s <= '9')
+	{
+		result = result * 10 + sign * (*s - '0');
+	}
+	else if (result)
+	{
+		break;
+	}
+}
+return (result);
+}
